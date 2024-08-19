f362b356554c0145f12dfed502a8e01c83cf9f89
z631200 <z631200@gmail.com>
2024-08-19 14:34:45 +0800

    solve merge conflict

--- ncu_emi/gpt/views.py
+++ ncu_emi/gpt/views.py
@@ -1,18 +1,35 @@
 import os
 import requests
-from rest_framework.views import APIView
+from rest_framework.views import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from openai import OpenAI
 from django.shortcuts import get_object_or_404
+from django.db import transaction
 
 from .models import Class
+from .models import Pptword
+from .serializers import PptWordSerializer
 
 
-class GPTResponseAPIView(APIView):
+class GPTResponseAPIView(GenericAPIView):
+    queryset = Pptword.objects.all()
+    serializer_class = PptWordSerializer
+
+    def get(self, request):
+        pptwords = self.get_queryset()
+        serializer = self.serializer_class(pptwords, many=True)
+        data = serializer.data
+        return Response(data, status=status.HTTP_200_OK)
+
     def post(self, request):
         data = request.data
-        try:           
+
+        try:  
+            if 'class_class' not in data:
+                return Response({'error': 'class_class is required.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+                 
             classes_id = data['class_class']
             
             classes = get_object_or_404(Class, class_id=classes_id)
@@ -39,6 +56,13 @@ class GPTResponseAPIView(APIView):
 
             messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
+            data['pptword_content'] = messages[0].content[0].text.value
+        
+            serializer = self.serializer_class(data=data)
+            serializer.is_valid(raise_exception=True)
+            with transaction.atomic():
+                serializer.save()
+                
             return Response({
                 "message": messages[0].content[0].text.value
             }, status=status.HTTP_200_OK)
